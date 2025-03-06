3295e743720a237e90807a311a4503c35222779c
dimsaratov <dimsaratov@inbox.ru>
2025-03-06 17:33:45 +0300

    fix del bin obj

--- XerLoader/obj/Debug/net8.0-windows/XerLoader.AssemblyInfo.cs
+++ XerLoader/obj/Debug/net8.0-windows/XerLoader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XerLoader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31af5951f6c81ff76da9807006b9600ae7999faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e0610cd648dfdc6afd5c0f68e24f1bea890e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("XerLoader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XerLoader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
